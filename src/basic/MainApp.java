a4a0682410425a4f75406ffd324516b9acb5d423
TranQuocThang <98342476+winsep@users.noreply.github.com>
2024-06-11 09:43:29 +0700

    Add files via upload

---
+++ src/basic/MainApp.java
@@ -0,0 +1,16 @@
+package basic;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				Scanner sc = new Scanner(System.in);
+				Object ten = sc.next();
+				Object tuoi = sc.nextInt();
+				Object diem = sc.nextDouble();
+
+			}
+
+		}
